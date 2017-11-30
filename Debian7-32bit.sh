cd6f042dbcf32b1ae96e9bbcd654f77345aa4214
rasta-team <irfan1037.psa@gmail.com>
2017-12-01 00:50:07 +0800

    Update Debian7-32bit.sh

--- Debian7-32bit.sh
+++ Debian7-32bit.sh
@@ -19,7 +19,7 @@ flag=0
 	
 #iplist="ip.txt"
 
-wget --quiet -O iplist.txt https://raw.githubusercontent.com/rasta-team/MyVPS/master/ip.txt
+wget --quiet -O iplist.txt https://raw.githubusercontent.com/rasta-team/autoscriptV1/master/ip.txt
 
 #if [ -f iplist ]
 #then
@@ -207,3 +207,43 @@ apt-get -y install webmin
 sed -i "s/ssl=1/ssl=0/g" /etc/webmin/miniserv.conf
 /etc/init.d/webmin restart
 service vnstat restart
+
+# Restart Service
+chown -R www-data:www-data /home/vps/public_html
+service cron restart
+service nginx start
+service php-fpm start
+service vnstat restart
+service openvpn restart
+service snmpd restart
+service ssh restart
+service dropbear restart
+service fail2ban restart
+service squid3 restart
+service webmin restart
+
+# info
+clear
+echo "Setup by Rasta-Team (Abe Pang)"  | tee -a log-install.txt
+echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.ovpn)"  | tee -a log-install.txt
+echo "OpenSSH  : 22, 143"  | tee -a log-install.txt
+echo "Dropbear : 80, 109, 110, 443"  | tee -a log-install.txt
+echo "Squid3   : 8080, 8000, 3128 (limit to IP SSH)"  | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
+echo "nginx    : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "----------"  | tee -a log-install.txt
+echo "----------"  | tee -a log-install.txt
+echo "Webmin   : http://$MYIP:10000/"  | tee -a log-install.txt
+echo "vnstat   : http://$MYIP:81/vnstat/"  | tee -a log-install.txt
+echo "MRTG     : http://$MYIP:81/mrtg/"  | tee -a log-install.txt
+echo "Timezone : Asia/Kuala Lumpur"  | tee -a log-install.txt
+echo "Fail2Ban : [on]"  | tee -a log-install.txt
+echo "IPv6     : [off]"  | tee -a log-install.txt
+echo "Status   : please type ./status to check user status"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "VPS REBOOT TIAP JAM 24.00 !"  | tee -a log-install.txt
+echo""  | tee -a log-install.txt
+echo "Please Reboot your VPS !"  | tee -a log-install.txt
+echo "================================================"  | tee -a log-install.txt
+echo "Script Created By ABE PANG"  | tee -a log-install.txt
